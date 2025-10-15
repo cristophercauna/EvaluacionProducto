b698b5bbca43852c74e9c9d6468a40f5099cf4ad
Farid <fchipanam@unjbg.edu.pe>
2025-10-15 09:36:37 -0500

    Modifico heapSort para contar comparaciones e intercambios

--- src/heapsort.cpp
+++ src/heapsort.cpp
@@ -2,18 +2,29 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+extern long long comparaciones_hs;
+extern long long intercambios_hs;
+
 void heapify(vector<long long>& arr, long long n, long long i) {
     long long largest = i;
     long long left = 2 * i + 1;
     long long right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
+    if (left < n) {
+        comparaciones_hs++;
+        if (arr[left] > arr[largest]){
+            largest = left;
+        }
+    }
 
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+    if (right < n) {
+        comparaciones_hs++;
+        if (arr[right] > arr[largest]) largest = right;
+    }
 
     if (largest != i) {
         swap(arr[i], arr[largest]);
+        intercambios_hs++;
         heapify(arr, n, largest);
     }
 }
@@ -22,6 +33,7 @@ void heapSort(vector<long long>& arr, long long n) {
         heapify(arr, n, i);
     for (long long i = n - 1; i > 0; i--) {
         swap(arr[0], arr[i]);
+        intercambios_hs++;
         heapify(arr, i, 0);
     }
 }
