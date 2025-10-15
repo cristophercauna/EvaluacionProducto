8039f1f0563136f9cc6a848b39aede75258ed360
cristophercauna <ccaunam@unjbg.edu.pe>
2025-10-14 23:08:29 -0500

    Actualizar heapsort.cpp: Ya modularizado heapsort se elimina el chrono ya que mas adelante se hara un main que comparara los tiempos, solo se deja la estructura de la funcion.

--- src/heapsort.cpp
+++ src/heapsort.cpp
@@ -1,31 +1,7 @@
-#include<iostream>
-#include<chrono>
-#include <random>
+#include "../include/heapsort.h"
 #include<vector>
+#include<algorithm>
 using namespace std;
-void heapify(vector<long long>& arr, long long n, long long i);
-void heapSort(vector<long long>& arr, long long n);
-int main(){
-    long long n;
-    cin >> n;
-    vector<long long> arr(n);
-    unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
-    mt19937 rng(seed);
-    uniform_int_distribution<long long> dist(0, 1000000);
-     for (long long i = 0; i < n; i++)
-        arr[i] = dist(rng);
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    heapSort(arr, n);
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout<<"Time difference = ";
-    cout<<chrono:: duration_cast<chrono::milliseconds>(end - begin).count()<<"[ms]"<<endl;
-    cout<<"Time difference = ";
-    cout<<chrono:: duration_cast<chrono::microseconds> (end - begin).count()<<"[us] "<<endl;
-    cout<<"Time difference = ";
-    cout<<chrono:: duration_cast<chrono:: nanoseconds> (end - begin).count()<< "[ns]" <<endl;
-
-    return 0;
-}
 void heapify(vector<long long>& arr, long long n, long long i) {
     long long largest = i;
     long long left = 2 * i + 1;
